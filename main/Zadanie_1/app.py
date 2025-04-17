f4e69efd74297139b12c16a7fa29054793d44be7
ailzakmi <anton_2000_19@mail.ru>
2025-04-18 01:07:04 +0500

    1,1

--- main/Zadanie_1/app.py
+++ main/Zadanie_1/app.py
@@ -1,4 +1,9 @@
+import datetime
 from flask import Flask
+
+counter_int = 0
+cars_mails = ["Lada", "Volkswagen", "Lotus", "Mitsubishi", "Reno", "Lamborghini"]
+cats_murlikin = ["Мейн-кун", "Сиамская", "Бурма", "Британская", "Сфинкс"]
 
 app = Flask(__name__)
 
@@ -6,5 +11,22 @@ app = Flask(__name__)
 def hello_world():
     return "Привет, мир!"
 
+@app.route('/test')
+def test():
+    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
+                      datetime.datetime.now()
+
+@app.route('/counter')
+def counter():
+    global counter_int
+    counter_int += 1
+    return str(counter_int)
+
+@app.route('/cars')
+def cars():
+    global cars_mails
+    return cars_mails;
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,
+            port=5555)
