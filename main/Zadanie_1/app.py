849b1e063a42f7aa154b1d6268f6518abda210a6
ailzakmi <anton_2000_19@mail.ru>
2025-04-18 01:33:32 +0500

    Закончил первое задание.

--- main/Zadanie_1/app.py
+++ main/Zadanie_1/app.py
@@ -1,9 +1,10 @@
 import datetime
+import random
 from flask import Flask
 
 counter_int = 0
 cars_mails = ["Lada", "Volkswagen", "Lotus", "Mitsubishi", "Reno", "Lamborghini"]
-cats_murlikin = ["Мейн-кун", "Сиамская", "Бурма", "Британская", "Сфинкс"]
+cats_murlikin = ["Мейн-кун", "Сиамская", "Бурма", "Британская", "Сфинкс", "Корниш-рекс", "Русская голубая", "Шотландская вислоухая", "Манчкин"]
 
 app = Flask(__name__)
 
@@ -24,8 +25,25 @@ def counter():
 
 @app.route('/cars')
 def cars():
-    global cars_mails
-    return cars_mails;
+    return ', '.join(cars_mails)
+
+@app.route('/cats')
+def cats():
+    return random.choice(cats_murlikin)
+
+@app.route('/get_time/now')
+def get_time_now():
+    current_time = str(datetime.datetime.now())
+    return "Текущее время: {" + current_time + "}"
+
+@app.route('/get_time/future')
+def get_time_future():
+    current_time_after_hour = str(datetime.datetime.now() + datetime.timedelta(hours=1))
+    return "Через час будет: {" + current_time_after_hour + "}"
+
+@app.route('/get_random_word')
+def get_random_word():
+    return "Книги нет, но вы держитесь."
 
 if __name__ == '__main__':
     app.run(debug=True,
